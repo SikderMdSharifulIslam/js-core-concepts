ff99255a634ba263af50707001ea3f672981949a
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-28 02:23:47 +0600

    premitive non-premitive data type

---
+++ premitive.js
@@ -0,0 +1,13 @@
+// premitive data type (value type)
+let data = 4;
+data = 'tormuj';
+data = false;
+
+// non-premitive data type (reference type)
+data = {};
+data = [];
+
+console.log(typeof data);
+
+// ekhane variable er moddhe j type er value rakha hobe last, shetai type dekhabe. 
+// doesn't matter first e ki deya hoisilo cz let diye shuru hoise.
